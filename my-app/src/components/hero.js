9a5cde529d68bbb30fe99d402a14442eac924ca2
unknown <guilhermetaliberti@gmail.com>
2024-02-12 09:33:04 -0300

    Cards do restante dos métodos implementados

--- my-app/src/components/hero.js
+++ my-app/src/components/hero.js
@@ -2,11 +2,11 @@ import "../Styles/Hero.css"
 import { Link } from 'react-router-dom'
 import CadastroCard from "./CadastroCard"
 import RequisiçãoCard from "./RequisiçãoCard"
+import Footer from "./Footer"
 
 export default function Hero() {
     
     return(
-
         <div className="hero">
             <div className="cadastroSection">
                 <Link to="/duxus/integrante/cadastro" className='link'>
@@ -26,6 +26,32 @@ export default function Hero() {
                     <RequisiçãoCard title="Integrante mais Usado" description="Retorne o integrante mais usado no período desejado."/>
                 </Link>
             </div>
+            <div className="requisiçãoSection">
+                <Link to="/duxus/time/teste3" className='link'>
+                    <RequisiçãoCard title="Time mais Comum" description="Retorne o time mais comum no período desejado."/>
+                </Link>
+            </div>
+            <div className="requisiçãoSection">
+                <Link to="/duxus/time/teste4" className='link'>
+                    <RequisiçãoCard title="Função mais Comum" description="Retorne a função mais comum no período desejado."/>
+                </Link>
+            </div>
+            <div className="requisiçãoSection">
+                <Link to="/duxus/time/teste5" className='link'>
+                    <RequisiçãoCard title="Franquia mais Famosa" description="Retorne a função mais comum no período desejado."/>
+                </Link>
+            </div>
+            <div className="requisiçãoSection">
+                <Link to="/duxus/time/teste5" className='link'>
+                    <RequisiçãoCard title="Contagem por Franquia" description="Retorne a contagem de franquias no período desejado."/>
+                </Link>
+            </div>
+            <div className="requisiçãoSection">
+                <Link to="/duxus/time/teste5" className='link'>
+                    <RequisiçãoCard title="Contagem por Função" description="Retorne a contagem de funções no período desejado."/>
+                </Link>
+            </div>
+            <Footer/>
         </div>
     )
 }
